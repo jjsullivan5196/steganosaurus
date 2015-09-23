47ecd741ac4603311fb2124b94476ec83e52188e
Spencer Belleau <spencer.belleau@gmail.com>
2015-09-22 20:11:12 -0700

    Oh noes, I broke it.

--- pillowTest.py
+++ pillowTest.py
@@ -10,7 +10,7 @@ def getRatio(tup):
 			smallest = element
 	list = []
 	for element in tup:
-		list.append(element/smallest)
+		list.append(int(element/smallest))
 	return tuple(list)
 #Distributes an int according to a tuple ratio
 #Returns a tuple with the int distributed inside according to the ratio
@@ -18,10 +18,11 @@ def distributeByRatio(amount, ratio):
 	total = 0
 	for num in ratio:
 		total += num
-	piece = amount/total
+	piece = int(amount/total)
 	l = []
 	for num in ratio:
 		l.append(piece * num)
+	l[0] += amount%total
 	return tuple(l)
 #Returns the sum of a tuple
 def sumTup(tup):
@@ -40,20 +41,42 @@ def getNewShift(oldShift):
 	if(ret > 100):
 		ret -= 200
 	return ret
+def getBrightness(pixel):
+	total = 0
+	for p in pixel:
+		total += p
+	#print("Brightness is " + str(int(total/3)))
+	return int(total/3)
+def normalizeBrightness(mainColor, b1, b2):
+	l = []
+	normalizeAmount = int((b1+b2)/6)
+	print("Adding " + str( normalizeAmount ) + " to brightness")
+	for p in mainColor:
+		l.append(p + normalizeAmount)
+	return tuple(l)
 ##END DEFS
 
 #Getting some basic info
 myImage = Image.open("testImage.jpg")
 xSize, ySize = myImage.size
 print("Width: " + str(xSize) + " Height: " + str(ySize) + " Total Pixel Number: " + str(xSize * ySize))
+
+file = open("hello.txt", 'r')
+fiList = list(file.readline())
+intFiList = []
+for e in fiList:
+	intFiList.append(ord(e))
+print(intFiList)
 #Determining info for pixel selection
 maxBytes = xSize * ySize / 3
-byteNum = 2000 #Change this to see byte dist
+byteNum = len(intFiList) #Change this to see byte dist
 if (byteNum > maxBytes):
 	exit()
 pixelSpace = int(xSize * ySize / byteNum)
-pixelShift = 0 #again, adjust for fun
+pixelShift = 0
 pixList = myImage.getdata()
+valIndex = 0
+
 copyImage = Image.new('RGB', myImage.size)
 newPixList = []
 xC, yC = 0, 0
@@ -61,21 +84,29 @@ dataPixList = []
 for n in range(len(pixList)):
 	x, y, z = pixList[n]
 	if((n + 1) % (pixelSpace + pixelShift) == 0):
-		newPixList.append((255, 0, 0))
-		pixelShift = getNewShift(pixelShift)
-		print(str(xC) + " " + str(yC))
-		if not (xC, yC) in dataPixList:
-			dataPixList.append((xC, yC))
-		else:
-			print("Pixel Value Repeated")
-			exit()
+		#Write the data to a pixel
+		if(valIndex < len(intFiList)):
+			newColor = distributeByRatio(intFiList[valIndex],getRatio((x, y, z)))
+			bright1, bright2 = getBrightness(pixList[n - 1]), getBrightness(pixList[n + 1])
+			newColorAdj = normalizeBrightness(newColor, bright1, bright2)
+			newPixList.append(newColorAdj)
+			print(newColorAdj)
+			print(sumTup(newColor))
+			print(chr(intFiList[valIndex]))
+			valIndex += 1
+			pixelShift = getNewShift(pixelShift)
+			#Not strictly necessary
+			if not (xC, yC) in dataPixList:
+				dataPixList.append((xC, yC))
+			else:
+				print("Pixel Value Repeated")
+				exit()
+		#######################
 	else:
 		newPixList.append((x, y, z))
 	xC += 1
 	if(xC > (xSize - 1)):
 		xC = 0
 		yC += 1
-print("Width: " + str(xSize) + " Height: " + str(ySize) + " Total Pixel Number: " + str(xSize * ySize))
-print(xSize * ySize)
 copyImage.putdata(newPixList)
 copyImage.save("out.png")
