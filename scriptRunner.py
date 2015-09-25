ccd74d8fd39748a231a39b183453c2ceea41fbf4
Spencer Belleau <spencer.belleau@gmail.com>
2015-09-24 18:08:34 -0700

    On a roll. Added a simple tool to check how big of a file one can store in an image as well as if there is already a file in the image. 99.99% accuracy. Theoretically if one stored a 999999 byte file (exactly) in an image where the first 3 pixels are exactly the same by default the checker will fail to determine that there is an image stored. Other than that it's accurate. Maybe I should expand the header and even store the file extension? Could be fun.

--- scriptRunner.py
+++ scriptRunner.py
@@ -1,5 +1,5 @@
 import os
-mode = str(input("Inject or Retrieve? (i/r)"))
+mode = str(input("Inject/Retrieve or Check? (i/r/c)"))
 if(mode == "i"):
 	image = str(input("Enter name of image file: "))
 	file = str(input("Enter name of file to inject: "))
@@ -9,5 +9,9 @@ elif(mode == "r"):
 	image = str(input("Enter name of image file: "))
 	outFile = str(input("Enter name of output file: "))
 	os.system("newAndImprovedReverser.py " + image + " " + outFile)
+elif(mode == "c"):
+	image = str(input("Enter name of image file: "))
+	mode = str(input("Max Bytes or Check for stored image?(m/c): "))
+	os.system("imageChecker.py " + image + " " + mode)
 else:
 	print("Invalid mode, please re-run launcher.")
